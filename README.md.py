6f79332ce0804f8399f3cb83ce5c66c0688da67c
Sky <58812660+xSkyyy@users.noreply.github.com>
2021-09-17 13:03:14 +0100

    Create README.md.py

---
+++ README.md.py
@@ -0,0 +1,14 @@
+## **Hi, I basically do nothing good. I work on a couple projects, but that's about it.**
+
+### Stuff I work on:
+
+- Ririka, a gulag-based osu! server: https://ririka.pw
+- EZPPFarm, also an osu! server, but nothing actual development related: https://ez-pp.farm
+- Kayo, a Discord Bot: https://kayo.pw
+- Mainly osu! videos and thumbnails: https://catlow.xyz/shop
+
+## Welcome to the beginning to the end, if you need me, contact me via:
+
+- Discord: Thomas#5365
+- Twitter: @Catlow_osu
+- Email: thomas@catlow.xyz
